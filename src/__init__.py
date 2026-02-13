511b96f06fcdfa003516b58691cdd5e5847496bc
sivaGU <30841391+sivaGU@users.noreply.github.com>
2026-02-13 10:48:19 -0500

    Add files via upload

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,12 @@
-"""PFAS Removal DFT GUI source package."""
+"""PFAS Removal DFT prediction module."""
+from .predict import predict_single, predict_batch, load_predictor, PredictResult
+from .orca_input import generate_workflow_inputs, generate_exchange_calculation_inputs, ORCAConfig
+from .orca_parser import parse_orca_output, calculate_exchange_energy, ORCAResults
+from .structure_utils import prepare_pfas_structure, prepare_cholestyramine_structure, prepare_complex_structure
+
+__all__ = [
+    "predict_single", "predict_batch", "load_predictor", "PredictResult",
+    "generate_workflow_inputs", "generate_exchange_calculation_inputs", "ORCAConfig",
+    "parse_orca_output", "calculate_exchange_energy", "ORCAResults",
+    "prepare_pfas_structure", "prepare_cholestyramine_structure", "prepare_complex_structure",
+]
